c11df5f317fd25aa8ce6b9a5c13f75968be4b519
distroteen <tayllerdesktop@gmail.com>
2025-09-04 16:53:18 -0300

    Create algebra.py

---
+++ algebra.py
@@ -0,0 +1,6 @@
+import sympy as sp
+
+def simplify_expr(expr): return sp.simplify(expr)
+def expand_expr(expr): return sp.expand(expr)
+def factor_expr(expr): return sp.factor(expr)
+def solve_equation(eq, var): return sp.solve(eq, var)
