12bbfc582d23956b51c2424c800642b3a8b4b112
distroteen <tayllerdesktop@gmail.com>
2025-09-04 16:54:22 -0300

    Create linear_algebra.py

---
+++ linear_algebra.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def determinant(matrix): return np.linalg.det(matrix)
+def inverse(matrix): return np.linalg.inv(matrix)
+def eigen(matrix): return np.linalg.eig(matrix)
+def svd(matrix): return np.linalg.svd(matrix)
